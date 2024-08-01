9505ba4bffc8dcb61309c453eddf14d3143bdc86
Shamimkhan1406 <shamim4khan0@gmail.com>
2024-08-01 22:55:57 +0530

    exp

---
+++ exp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class exp {
+    public static void main(String[] args) {
+        double v=4,u=2,a=1,s=2;
+        System.out.println((v*v-u*u)/(2*(a*s)));
+        int x=7;
+        int A=7*(49/7)+35/7;
+        System.out.println(A);
+    }
+}
