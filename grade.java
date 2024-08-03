aa0bab6c6e6bce9e58b256df9e90106b0dbbce9c
Shamimkhan1406 <shamim4khan0@gmail.com>
2024-08-04 00:02:45 +0530

    grade

---
+++ grade.java
@@ -0,0 +1,12 @@
+public class grade {
+    public static void main(String[] args)
+    {
+        char grade = 'B';
+        grade= (char)(grade + 8);
+        System.out.println(grade);
+        // decripting the grade
+        grade= (char)(grade - 8);
+        System.out.println(grade);
+        
+    }
+}
