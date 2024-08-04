d405426ed11df874f48edf20dc0e476da9446d0c
Shamimkhan1406 <shamim4khan0@gmail.com>
2024-08-05 00:46:42 +0530

    literals

---
+++ literals.java
@@ -0,0 +1,11 @@
+public class literals {
+    public static void main(String[] args)
+    {
+        byte age = 34;
+        char ch='A';
+        float f1=5.6f;
+        System.out.println(age);
+        String A="shamim";
+        System.out.print(A);
+    }
+}
