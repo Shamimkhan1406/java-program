dff73d7542557d620620b9d9f53805378da92b24
Shamimkhan1406 <shamim4khan0@gmail.com>
2024-08-19 22:30:19 +0530

    Operator

---
+++ operator.java
@@ -0,0 +1,7 @@
+public class operator {
+    public static void main(String[] args){
+        System.out.println(55>2 && 55>33);
+        float a=7/4.0f*9/2.0f;
+        System.out.println(a);
+    }
+}
